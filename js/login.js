984ef843929d2898f2c9ae1b5b585c27ac6174c9
André Henrique <you@example.com>
2020-02-22 21:48:40 -0300

    to cansado

--- js/login.js
+++ js/login.js
@@ -1,6 +1,7 @@
 var btnSignin = document.querySelector("#signin");
 var btnSignup = document.querySelector("#signup");
 var body = document.querySelector("body");
+var cpf = document.getElementById("cpf")
 
 btnSignin.addEventListener("click", function (){
     body.className = "sign-in-js";
@@ -9,3 +10,13 @@ btnSignin.addEventListener("click", function (){
 btnSignup.addEventListener("click", function (){
     body.className= "sign-up-js";
 })
+
+cpf.addEventListener('keyup', (event) => {
+    if (isNaN(cpf.value)) {
+        cpf.value = cpf.value.substring(0, (cpf.value.length - 1));
+    }
+    if (cpf.value.length >= 11) {
+        cpf.value = cpf.value.substring(0, 11);
+    }
+});
+
