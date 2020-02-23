c407bc87cd492d4645b0a857ac64125e15f926c3
André Henrique <you@example.com>
2020-02-22 22:25:05 -0300

    to cansado 2

--- js/login.js
+++ js/login.js
@@ -2,6 +2,7 @@ var btnSignin = document.querySelector("#signin");
 var btnSignup = document.querySelector("#signup");
 var body = document.querySelector("body");
 var cpf = document.getElementById("cpf")
+var dta = document.getElementById("dtaNascimento")
 
 btnSignin.addEventListener("click", function (){
     body.className = "sign-in-js";
@@ -19,4 +20,13 @@ cpf.addEventListener('keyup', (event) => {
         cpf.value = cpf.value.substring(0, 11);
     }
 });
+
+// dta.addEventListener('keyup', (event) => {
+//     if (isNaN(dta.value)) {
+//         dta.value = dta.value.substring(0, (dta.value.length - 1));
+//     }
+//     if (dta.value.length >= 8) {
+//         dta.value = dta.value.substring(0, 8);
+//     }
+// });
 
